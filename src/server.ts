365ea9bfec09b6840169dfc4e1b874ac545d689d
ryanmarinhodev <ryanmarinhodev@gmail.com>
2024-10-24 14:48:33 -0300

    atualizando o server port

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,34 @@
-import userRouter from "./routes/userRouter";
-import express from 'express'
-import videosRoutes from "./routes/videosRoutes";
-import { config } from 'dotenv'
+import userRouter from './routes/userRouter';
+import express from 'express';
+import videosRoutes from './routes/videosRoutes';
+import { config } from 'dotenv';
 
 config();
 const app = express();
 
 const cors = require('cors');
 
-app.use(function(req,res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, OPTIONS");
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'POST, GET, PATCH, DELETE, OPTIONS'
+  );
   next();
 });
 
-app.use(cors())
+app.use(cors());
 
+app.use('/user', userRouter);
+app.use('/videos', videosRoutes);
 
-app.use('/user', userRouter)
-app.use('/videos', videosRoutes)
+console.log(process.env.SECRET);
 
-console.log(process.env.SECRET)
-
-
-
-app.listen(4000, () => {
-  console.log('Servidor rodando na porta 4000');
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
