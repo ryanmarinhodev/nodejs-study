3b421b6450b4352243ed5248ad94fcce05083f95
ryanmarinhodev <ryanmarinhodev@gmail.com>
2024-10-25 16:17:43 -0300

    mudando a api para localhost

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = localStorage.getItem('token');
 
 const api = axios.create({
-  baseURL: 'https://nodejs-study-production.up.railway.app', //Trocar esse endPoint para o novo quando hospedar essa api node
+  baseURL: 'http://localhost:4001', //Trocar esse endPoint para o novo quando hospedar essa api node
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
