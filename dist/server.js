8310dcc84d56c6c611f4e583c8e57a5498d314ed
ryanmarinhodev <ryanmarinhodev@gmail.com>
2024-10-25 12:08:00 -0300

    mudando o typescript para dependencia

--- dist/server.js
+++ dist/server.js
@@ -11,15 +11,16 @@ const dotenv_1 = require("dotenv");
 const app = (0, express_1.default)();
 const cors = require('cors');
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, OPTIONS");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
     next();
 });
 app.use(cors());
 app.use('/user', userRouter_1.default);
 app.use('/videos', videosRoutes_1.default);
 console.log(process.env.SECRET);
-app.listen(4000, () => {
-    console.log('Servidor rodando na porta 4000');
+const PORT = process.env.PORT || 4001;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
