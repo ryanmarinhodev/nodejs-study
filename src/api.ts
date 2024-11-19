4969fdffad15344db31e835f526eaa094bb85a01
ryanmarinhodev <ryanmarinhodev@gmail.com>
2024-11-19 15:21:39 -0300

    adicionando s no http, talvez seja esse o problema

--- src/api.ts
+++ src/api.ts
@@ -3,11 +3,7 @@ import axios from 'axios';
 const token = localStorage.getItem('token');
 
 const api = axios.create({
-<<<<<<< HEAD
-  baseURL: 'https://localhost:4001', //Trocar esse endPoint para o novo quando hospedar essa api node
-=======
-  baseURL: 'http://localhost:4001', //https://nodejs-study-production.up.railway.app/ //http://localhost:4001
->>>>>>> c8f0cf8101cb7e1c19c1dc1153c4d6c209683254
+  baseURL: 'https://localhost:4001', //https://nodejs-study-production.up.railway.app/ //http://localhost:4001
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
